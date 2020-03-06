b77df23df375eeb95878c3e1f9199fdebfffcd8b
Ketho <sghs17@outlook.com>
2020-03-06 05:53:57 +0100

    InterfaceIcons

---
+++ examples/InterfaceIcons.lua
@@ -0,0 +1,30 @@
+-- https://github.com/ketho-wow/LargerMacroIconSelection/blob/master/Data/FileDataRetail.lua
+local parser = require "wowtoolsparser"
+local output = "out/InterfaceIcons.lua"
+
+local function main(BUILD)
+	local filedata = parser.ReadListfile(true)
+	local sorted = {}
+	for fdid, path in pairs(filedata) do
+		local _, _, icon = path:find("interface/icons/(.+)%.blp$")
+		if icon then
+			table.insert(sorted, {fdid, icon})
+		end
+	end
+	table.sort(sorted, function(a, b)
+		return a[1] < b[1]
+	end)
+
+	local fs = '[%d]="%s",\n'
+	print("writing to "..output)
+	local file = io.open(output, "w")
+	file:write("local InterfaceIcons = {\n")
+	for _, tbl in pairs(sorted) do
+		file:write(fs:format(table.unpack(tbl)))
+	end
+	file:write("}\n")
+	file:close()
+	print("finished")
+end
+
+main()
